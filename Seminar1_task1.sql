8cecfeb6606ff0c5d5c725b7fbe9aeca17711334
Iliya <ilusann@inbox.ru>
2023-04-09 17:34:59 +0300

    Добавлено первое задание из семинара 1

---
+++ Seminar1_task1.sql
@@ -0,0 +1,5 @@
+INSERT students (name_student, phone_number, email, date_of_birth)
+VALUES
+	("Анна", 1234567890, 'anna@gmail.con', '23.04.1990'),
+    ("Алексей", 1122334456, 'alex@gmail.con', '06.11.1995'),
+    ("Ярослав", 3456712390 , 'ya@gmail.con', '14.02.1999');
